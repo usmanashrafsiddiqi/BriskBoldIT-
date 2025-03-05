2011b256d5ca2477b6d0155173be022eb515f4d6
usman siddiqi <usmansiddiqi2000@gmail.com>
2025-03-05 17:52:42 +0530

    changed number for whatapp bot

--- src/pages/WhatsAppChat.jsx
+++ src/pages/WhatsAppChat.jsx
@@ -8,7 +8,7 @@ const WhatsAppChat = () => {
     <div>
       {/* WhatsAppWidget is a component that renders the floating button */}
       <WhatsAppWidget
-        phoneNumber="+917051087741" // Replace with your WhatsApp number including the country code
+        phoneNumber="+16474463712" // Replace with your WhatsApp number including the country code
         message="Hello" // Default message to appear in the chat input
         companyName="BriskBold AI & Automation"
         textReplyTime="Usually replies in a few minutes"
